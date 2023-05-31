25db359415fe03dc46d3ca37955995ea54b7b08e
Jake Leahy <jake@leahy.dev>
2023-05-31 14:35:53 +1000

    Automatically define SSL so the user doesn't need to (#5)
    * Automatically define SSL so the user doesn't need to
    
    * Remove echo statement that was left in from the fix PR

--- src/pantry.nim
+++ src/pantry.nim
@@ -1,3 +1,5 @@
+{.define: ssl.}
+
 import std/[
   httpclient,
   asyncdispatch,
@@ -244,7 +246,6 @@ proc request(pc: PantryClient | AsyncPantryClient, path: string,
       "Content-Type": "application/json"
     }
   )
-  echo resp.headers
   let msg = await resp.body
   case resp.code.int
   of 200..299:
